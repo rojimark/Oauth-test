8730db5cdb089ce3a98d1619d06cbffc94c8635f
rojimark <25628846+rojimark@users.noreply.github.com>
2018-03-06 03:20:10 +0800

    Delete keys.js

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-//add this gile to .gitignore
-
-modeul.exports = {
-  google: {
-    clientID:'141981017502-kq17nnponrnc7ds3uhd73nid0ku6q3hk.apps.googleusercontent.com',
-    clientSecret: 'U0vbOq5rhZDmIREX5sxDNwMn'
-  }
-};
